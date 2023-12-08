d255e61cec472b864801189287cd36b0faa998b6
bilalbadar01 <bilalee4725@gmail.com>
2023-12-08 21:28:19 +0500

    This commit modifies Pipeline to use path-based filtering

--- appointments/app.py
+++ appointments/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 appointments = [
-  { 'id': "1",'doctor': "1", 'date': "21 Nov 2023", 'rating':"Good"  },
+  { 'id': "1",'doctor': "1", 'date': "21 Nov 2023", 'rating':"Bad"  },
   { 'id': "2",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
   { 'id': "3",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
   { 'id': "4",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
